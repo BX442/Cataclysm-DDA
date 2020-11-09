fe6c11d47fee3b705d33f6ed6c801b61f30fe229
BX442 <35437046+BX442@users.noreply.github.com>
2020-11-09 18:30:08 +0900

    Dist.bat update

--- msvc-full-features/distribute.bat
+++ msvc-full-features/distribute.bat
@@ -1,10 +1,10 @@
 @echo off
-mkdir distribution_%Date:~0,2%.%Date:~3,2%.%Date:~6,4%_%TIME:~0,2%.%TIME:~0,2%.%TIME:~6,2%
-xcopy dll\*.dll distribution\
-xcopy /s /e /i ..\data distribution\data
-xcopy /s /e /i ..\config distribution\config
-xcopy /s /e /i ..\gfx distribution\gfx
-xcopy /s /e /i ..\lang\mo distribution\lang\mo
-copy ..\Cataclysm*.exe distribution\
-echo Distribution files has been put into `distribution_%Date:~0,2%.%Date:~3,2%.%Date:~6,4%_%TIME:~0,2%.%TIME:~0,2%.%TIME:~6,2%\' directory.
-pause
+SET DIR=%Date:~0,2%.%Date:~3,2%.%Date:~6,4%_%TIME:~0,2%.%TIME:~3,2%.%TIME:~6,2%
+mkdir distribution_%DIR%
+xcopy dll\*.dll distribution_%DIR%\
+xcopy /s /e /i ..\data distribution_%DIR%\data
+xcopy /s /e /i ..\config distribution_%DIR%\config
+xcopy /s /e /i ..\gfx distribution_%DIR%\gfx
+xcopy /s /e /i ..\lang\mo distribution_%DIR%\lang\mo
+copy ..\Cataclysm*.exe distribution_%DIR%\
+
